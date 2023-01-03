183c3f0e71c75d9d0d5c2a15135e4e73a6aab135
Muhammed Rashid N <muhammed916rashid@gmail.com>
2023-01-03 13:04:24 +0530

    PORT 3001 to 3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule , { cors: true } );
-  // app.get((req : any , res :any)=>{
-  //   res.sendFile(`${__dirname}/build/index.html`)
-  // })
-  await app.listen(3001);
+  await app.listen(3000);
   
 }
 bootstrap();
